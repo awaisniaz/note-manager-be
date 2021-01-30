962aee113ca0e272849233712c8986e9c1901413
owais <owais.niaz@targetofs.com>
2021-01-30 23:42:07 +0500

    some changes in registration api

--- router/userRouter.js
+++ router/userRouter.js
@@ -3,12 +3,12 @@ const router = express.Router()
 const userModel = require('../models/user')
 
 router.post('/login', (req, res) => {
-    userModel.find((err, data) => {
-        if (err) {
-            res.send('Sorry try again')
+    userModel.findOne({ email: req.body.email, password: req.body.password }, (err, data) => {
+        if (data) {
+            res.send({ message: 'Login Successfully', response: true })
         }
         else {
-            res.send(data)
+            res.send({ message: 'Try Again', response: false })
         }
     })
 })
@@ -20,24 +20,34 @@ router.post('/registration', (req, res) => {
         confirm_password: req.body.confirm_password,
         profile_picture: req.body.profile_picture
     })
-    const data = userModel.find({ email: req.body.email })
-    newUser.save((err, status) => {
-        if (err) {
-            res.send(err.message)
-            res.end()
+    userModel.findOne({ email: req.body.email }, (err, userExist) => {
+        if (userExist == null) {
+            newUser.save((err, data) => {
+                if (err) {
+                    res.send({ err, user }).status(202)
+                    res.end()
+                }
+                else {
+                    res.send({
+                        message: "Successfully Created User",
+                        success: true,
+                    })
+                }
+            })
         }
         else {
             res.send({
-                message: "Successfully Created User",
-                success: true
+                message: "User Email Is already Exist",
+                success: false
             })
         }
     })
+
 })
 router.delete('/delete', (req, res) => {
     userModel.deleteMany({}, (err, data) => {
         if (err) {
-            res.send('Sorry Try Again')
+            res.send('All User Deleted')
             res.end()
         }
         else {
