c2b897a308f19bc11665ee644ba5a0b8b89f7e4c
Muhammad Awais Niaz <awaisniaz1995@gmail.com>
2022-03-19 23:24:33 +0500

    Update document.js

--- models/document.js
+++ models/document.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
 const user = new mongoose.Schema({
-    
-    
     name: { type: String, minlength: 5 },
     type: { type: String },
     size: { type: Number },
