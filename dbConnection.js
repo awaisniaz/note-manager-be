2239df0120cb01dd0cfcc2157b11fe9243a04e0f
Muhammad Awais Niaz <awaisniaz1995@gmail.com>
2022-03-21 22:49:33 +0500

    Update dbConnection.js

--- dbConnection.js
+++ dbConnection.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true });
-
 const db = mongoose.connection;
 db.on('error', (error) => {
     console.log('I am Error Please Fix me')
 })
-
 db.once('open', () => {
     console.log('Congratulations')
 })
