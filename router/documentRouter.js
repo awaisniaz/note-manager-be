62f364f84130b7202e41e307f9ff6cef7861be65
Muhammad Awais Niaz <awaisniaz1995@gmail.com>
2022-03-26 22:51:53 +0500

    Update documentRouter.js

--- router/documentRouter.js
+++ router/documentRouter.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const documentModel = require('../models/document')
+
 router.post('/upload/document', (req, res) => {
 })
+
 router.get('/document', (req, res) => {
 
 })
