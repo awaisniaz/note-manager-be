5446c6a36dd9e3644ecc5f2c13f3854e9905ef0a
Muhammad Awais Niaz <awaisniaz1995@gmail.com>
2022-03-30 21:16:08 +0500

    Update document.js

--- models/document.js
+++ models/document.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+
 const user = new mongoose.Schema({
     name: { type: String, minlength: 5 },
     type: { type: String },
@@ -7,5 +8,6 @@ const user = new mongoose.Schema({
     date: { type: Date },
     picture: { type: String }
 })
+
 const userModel = mongoose.model('User', user)
 module.exports = userModel
